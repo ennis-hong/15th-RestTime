a668e31fde93a1568e3952a26fa98f4a62131bb4
Adoraxuu <adoraxu01@gmail.com>
2023-12-18 12:37:18 +0800

    add create_products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,19 +1,52 @@
 # frozen_string_literal: true
 
 class ProductsController < ApplicationController
-  def index; end
+  before_action :find_product, only: %i[show edit update destroy]
+
+  def index
+    @products = Product.all.order(id: :desc)
+  end
 
   def show; end
 
-  def new; end
+  def new
+    @product = Product.new
+  end
 
-  def create; end
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path, notice: '新增商品成功'
+    else
+      render :new
+    end
+  end
 
   def edit; end
 
-  def update; end
+  def update
+    # if @product.update(product_params)
+    #   redirect_to product_path(@product), notice: '更新成功'
+    # else
+    #   render :edit
+    # end
+  end
 
-  def destroy; end
+  def destroy
+    # find_product
+    # @product.destroy
+    # redirect_to root_path, notice: '商品已刪除'
+  end
 
   def search; end
+
+  private
+
+  def find_product
+    @product = Product.find(params[:id])
+  end
+
+  def find_owned_product
+    @product = current_user.products.find(params[:id])
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,8 @@
 class Product < ApplicationRecord
   validates :title, presence: true
   validates :price, numericality: { greater_than: 0 }
+
+  def destroy
+    update(deleted_at: Time.current)
+  end
 end
