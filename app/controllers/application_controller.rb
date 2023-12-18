ad9485240fbe07bcbe76dbff73981466f8cdf6a0
Adoraxuu <adoraxu01@gmail.com>
2023-12-18 19:43:39 +0800

    add i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
-class ApplicationController < ActionController::Base
 
+class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   def not_found
     render file: Rails.public_path.join('404.html'),
-          status: 404,
-          layout: false
+           status: 404,
+           layout: false
+  end
+
+  def default_url_options
+    { lang: I18n.locale }
+  end
+
+  def switch_locale(&)
+    lang = params[:lang] || I18n.default_locale
+    I18n.with_locale(lang, &)
   end
 end
