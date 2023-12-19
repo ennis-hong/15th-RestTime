ece785ea3e036e4be6badb7bc620b4575f6fde72
Dorenchen <f852doren@gamil.com>
2023-12-19 15:21:27 +0800

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
 
     root 'products#index'
 
+    get '/about', to: 'pages#about'
+    get '/choose_us', to: 'pages#choose_us'
+    get '/join_us', to: 'pages#join_us'
+    get '/contact_us', to: 'pages#contact_us'
+    get '/terms', to:'pages#terms'
+    get '/privacy', to:'pages#privacy'
+    get '/refund_policy', to:'pages#refund_policy'
     devise_for :users, controllers: { sessions: 'users/sessions' }
   end
 end
