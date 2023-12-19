710eb2619abb4ed11984c2bf85822c94f5dfe8a5
Adoraxuu <adoraxu01@gmail.com>
2023-12-19 15:56:00 +0800

    rename zh-TW to tw and add ActiveRecord attributes in tw.yml

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module RestTime
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
-    config.i18n.default_locale = "zh-TW"
+    config.i18n.default_locale = "tw"
 
 
 
