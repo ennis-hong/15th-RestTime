958e346b8971a5507965fb44c64d1ee4aef51108
Dorenchen <f852doren@gmail.com>
2023-12-27 16:46:52 +0800

    Third party google login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,10 +2,10 @@
 
 module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-    skip_before_action :verify_authenticity_token, only: [:google_oauth2]
+  skip_before_action :verify_authenticity_token, only: [:google_oauth2]
 
-    def google_oauth2
-      @user = User.from_omniauth(request.env['omniauth.auth'])
+  def google_oauth2
+    @user = User.from_omniauth(request.env['omniauth.auth'])
 
       if @user && @user.persisted?
         sign_in_and_redirect @user, event: :authentication
@@ -15,4 +15,5 @@ module Users
       end
     end
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,11 @@ class User < ApplicationRecord
   def self.from_omniauth(access_token)
     data = access_token.info
     user = User.find_by(email: data['email'])
+  end
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.find_by(email: data['email'])
 
     user ||= User.create(email: data['email'],
                          password: Devise.friendly_token[0, 20])
