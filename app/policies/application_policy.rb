24659fc6e4e1c02064608e154a1d951a3cc2b584
Adoraxuu <adoraxu01@gmail.com>
2023-12-27 18:44:23 +0800

    adjust product policy and application policy

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -8,26 +8,38 @@ class ApplicationPolicy
     @record = record
   end
 
+  def show_vendor_link?
+    user.vendor? || user.admin?
+  end
+
+  def show_create_shop_link?
+    user.admin?
+  end
+
+  def show_product_edit_destroy_link?
+    user&.own?(record) || user&.admin?
+  end
+end
+
   private
 
-  def create_default_shop
-    return false unless user.vendor? && user.shop.nil?
+def create_default_shop
+  return false unless user.vendor? && user.shop.nil?
 
-    # robocop會自動幫我改成反述法，如果 user.vendor & 沒有店家就執行以下
+  # robocup會自動幫我改成反述法，如果 user.vendor & 沒有店家就執行以下
 
-    shop = Shop.new(
-      title: user.email,
-      description: 'Default Description',
-      district: 'Default District',
-      city: 'Default City',
-      street: 'Default Street',
-      contact: 'Default Contact',
-      tel: '000000000',
-      contactphone: '000000000'
-    )
+  shop = Shop.new(
+    title: user.email,
+    description: 'Default Description',
+    district: 'Default District',
+    city: 'Default City',
+    street: 'Default Street',
+    contact: 'Default Contact',
+    tel: '000000000',
+    contactphone: '000000000'
+  )
 
-    user.shop = shop
-    shop.save
-    true # 返回 true 表示店鋪已經被創建
-  end
+  user.shop = shop
+  shop.save
+  true # 返回 true 表示店鋪已經被創建
 end
