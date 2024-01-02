edc29ebaa83252ea5f09cdf776b579bfe01c8e02
Dorenchen <f852doren@gamil.com>
2024-01-02 11:23:33 +0800

    使用rubocop整理

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,7 @@ class User < ApplicationRecord
 
   def self.from_omniauth(access_token)
     data = access_token.info
-    user = User.find_by(email: data['email'])
+    User.find_by(email: data['email'])
   end
 
   def self.from_omniauth(access_token)
