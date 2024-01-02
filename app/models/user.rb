f8edcc15c36a6eef01c94d39df2f1fefec9bb5a3
Dorenchen <f852doren@gmail.com>
2024-01-02 12:08:16 +0800

    移除重複方法

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,11 +28,6 @@ class User < ApplicationRecord
 
   def self.from_omniauth(access_token)
     data = access_token.info
-    User.find_by(email: data['email'])
-  end
-
-  def self.from_omniauth(access_token)
-    data = access_token.info
     user = User.find_by(email: data['email'])
 
     user ||= User.create(email: data['email'],
