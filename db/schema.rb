08fe04adc2a276bcd24083d70e893d7661a30dfc
Dorenchen <f852doren@gamil.com>
2024-01-05 20:11:57 +0800

    fix:(登入頁面)

--- db/schema.rb
+++ db/schema.rb
@@ -142,7 +142,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_06_175513) do
     t.datetime "remember_created_at"
     t.string "provider"
     t.string "uid"
-
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
