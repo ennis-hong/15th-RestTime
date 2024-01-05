695ef6b4778fe4c9237f3a9f24b012eb99cb17f8
Dorenchen <f852doren@gamil.com>
2024-01-05 20:11:57 +0800

    fix:(登入頁面)

--- db/schema.rb
+++ db/schema.rb
@@ -24,34 +24,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_30_112819) do
     t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
   end
 
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum"
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
   create_table "bookings", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "product_id", null: false
@@ -67,8 +39,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_30_112819) do
     t.bigint "shop_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "content"
-    t.datetime "deleted_at"
     t.integer "rating", default: 0, null: false
     t.index ["shop_id"], name: "index_comments_on_shop_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
@@ -104,8 +74,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_30_112819) do
     t.datetime "payment_date"
     t.string "payment_type"
     t.string "payment_type_charge_fee"
-    t.string "rtn_code"
-    t.string "rtn_msg"
+    t.string "return_code"
+    t.string "return_msg"
     t.index ["cancelled_at"], name: "index_orders_on_cancelled_at"
     t.index ["product_id"], name: "index_orders_on_product_id"
     t.index ["serial"], name: "index_orders_on_serial"
@@ -128,16 +98,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_30_112819) do
     t.bigint "shop_id"
     t.index ["deleted_at"], name: "index_products_on_deleted_at"
     t.index ["shop_id"], name: "index_products_on_shop_id"
-  end
-
-  create_table "ratings", force: :cascade do |t|
-    t.integer "score", default: 0
-    t.bigint "user_id", null: false
-    t.bigint "shop_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["shop_id"], name: "index_ratings_on_shop_id"
-    t.index ["user_id"], name: "index_ratings_on_user_id"
   end
 
   create_table "service_times", force: :cascade do |t|
@@ -180,16 +140,13 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_30_112819) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "role"
     t.string "provider"
     t.string "uid"
-
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "products"
   add_foreign_key "bookings", "users"
   add_foreign_key "comments", "shops"
@@ -200,8 +157,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_30_112819) do
   add_foreign_key "orders", "shops"
   add_foreign_key "orders", "users"
   add_foreign_key "products", "shops"
-  add_foreign_key "ratings", "shops"
-  add_foreign_key "ratings", "users"
   add_foreign_key "service_times", "shops"
   add_foreign_key "shops", "users"
 end
