e12ea79f7939686385714cd49e060f31e2dc43a8
Adoraxuu <adoraxu01@gmail.com>
2024-01-06 13:36:36 +0800

    chore(nav):整理不必要的程式

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -3,10 +3,8 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="sidebar"
 export default class extends Controller {
   static targets = ["menu"];
-  isMenuVisible = false;
 
   toggle() {
-    this.isMenuVisible = !this.isMenuVisible;
-    this.menuTarget.classList.toggle("hidden", !this.isMenuVisible);
+    this.menuTarget.classList.toggle("hidden", this.isMenuVisible);
   }
 }
