40f776915737afa5165d97c3ef11e44b6275581b
Adoraxuu <adoraxu01@gmail.com>
2024-01-11 13:30:31 +0800

    feat(daisyUI):調整前後台主色彩，套用daisyUI主題自訂

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,34 @@ module.exports = {
   ],
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["autumn"],
+    themes: [
+      {
+        frontstage: {
+          "primary": "#4a7c59",
+          "secondary": "#a98467",
+          "accent": "#abc178",
+          "neutral": "#dde5b6",
+          "base-100": "#f3f4f6",
+          "info": "#f0ead2",
+          "success": "#b5c99a",
+          "warning": "#f4a259",
+          "error": "#bc4b51",
+        },
+      },
+      {
+        backstage: {
+          "primary": "#6c584c",
+          "secondary": "#606C38",
+          "accent": "#a98467",
+          "neutral": "#DDA15E",
+          "base-100": "#f5f5f4",
+          "info": "#FEFAE0",
+          "success": "#a4ac86",
+          "warning": "#f4e285",
+          "error": "#bc4b51",
+        }
+      },
+      "autumn",
+    ],
   },
 };
