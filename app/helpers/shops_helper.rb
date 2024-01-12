82545402e7763fedfb99be35f38cc69e18a49267
Adoraxuu <adoraxu01@gmail.com>
2024-01-12 12:21:21 +0800

    fetch(vendor/index):修改後台圖片，寫方法在shop_helper

--- app/helpers/shops_helper.rb
+++ app/helpers/shops_helper.rb
@@ -35,4 +35,12 @@ module ShopsHelper
       }
     end
   end
+
+  def shop_cover(shop, variant = :thumb)
+    if shop.cover.attached?
+      image_tag shop.cover.variant(variant)
+    else
+      image_tag 'shops/default_cover.png'
+    end
+  end
 end
