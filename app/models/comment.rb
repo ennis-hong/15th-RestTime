6601ea2fd83eff8683f135c16e179f33c6cb5553
Adoraxuu <adoraxu01@gmail.com>
2024-01-13 21:03:49 +0800

    fetch(comment):增加i18n,減少驗證使用者

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,5 +5,4 @@ class Comment < ApplicationRecord
   belongs_to :shop
   has_rich_text :body
   validates :rating, numericality: { in: 0..5 }, presence: true
-  validates :user_id, uniqueness: { scope: :shop_id, message: 'You can only leave one comment per shop.' }
 end
