a3cd183a281d0267c3381c1fe649fdc7040fd264
Dorenchen <f852doren@gmail.com>
2024-01-15 20:06:37 +0800

    chore(sweetalert)修改成功登入

--- app/javascript/controllers/sweetalert2_controller.js
+++ app/javascript/controllers/sweetalert2_controller.js
@@ -2,12 +2,25 @@ import { Controller } from "@hotwired/stimulus";
 import Swal from "sweetalert2";
 // Connects to data-controller="sweetalert2"
 export default class extends Controller {
+  static targets = ["abc"];
+
   connect() {
     Swal.fire({
       title: "success",
-      text: "Do you want to continue",
+      text: this.abcTarget.dataset.content,
       icon: "success",
       confirmButtonText: "Cool",
     });
   }
 }
+// export default class extends Controller {
+//   connect() {
+//     Swal.fire({
+//       title: "success",
+//       text: "flash[type]",
+//       icon: "success",
+//       confirmButtonText: "Cool",
+//     });
+//   }
+// }
+// console.log(this.abcTarget.dataset.content);
