67f3f71d51c721641340ffbef276bca07d950fe7
Dorenchen <f852doren@gamil.com>
2024-01-16 19:15:46 +0800

    chore(sweetalert)

--- app/javascript/controllers/sweetalert2_controller.js
+++ app/javascript/controllers/sweetalert2_controller.js
@@ -1,26 +1,17 @@
 import { Controller } from "@hotwired/stimulus";
 import Swal from "sweetalert2";
-// Connects to data-controller="sweetalert2"
+
 export default class extends Controller {
   static targets = ["abc"];
 
   connect() {
     Swal.fire({
-      title: "success",
+      position: "top-end",
       text: this.abcTarget.dataset.content,
       icon: "success",
-      confirmButtonText: "Cool",
+      showConfirmButton: false,
+      width: "200px",
+      timer: 1500,
     });
   }
 }
-// export default class extends Controller {
-//   connect() {
-//     Swal.fire({
-//       title: "success",
-//       text: "flash[type]",
-//       icon: "success",
-//       confirmButtonText: "Cool",
-//     });
-//   }
-// }
-// console.log(this.abcTarget.dataset.content);
