70e27e1d0ea2feadaeae7ed13dd4e4da176e08d5
EnnisHong <kugi7821@gmail.com>
2024-01-16 11:16:50 +0800

    fix:判斷可預約時段排除已完成及取消訂單

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -7,8 +7,10 @@ module Api
 
       def available_slots
         date_range = params[:booking_date].to_date.beginning_of_day..params[:booking_date].to_date.end_of_day
+        orders = @shop.orders.where(service_date: date_range)
+                      .where.not(status: %i[completed cancelled])
         booking_service = BookingService.new(@shop.service_times,
-                                             @shop.orders.where(service_date: date_range),
+                                             orders,
                                              @shop.products.find_by(id: params[:product]))
         available_slots = booking_service.display_available_slots(DateTime.parse(params[:booking_date],
                                                                                  '%Y/%m/%d %H:%M'))
