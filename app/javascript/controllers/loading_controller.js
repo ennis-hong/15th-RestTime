ce2999de82bb4a9aa9f1061bc161883381a5fe87
EnnisHong <kugi7821@gmail.com>
2024-01-19 15:22:15 +0800

    chore: 增加跳轉loading

---
+++ app/javascript/controllers/loading_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "@hotwired/stimulus";
+import Swal from "sweetalert2";
+
+// Connects to data-controller="loading"
+export default class extends Controller {
+  disconnect() {
+    Swal.close();
+  }
+
+  loading() {
+    const { message } = this.element.dataset;
+    Swal.fire({
+      title: message,
+      allowEscapeKey: false,
+      allowOutsideClick: false,
+      showConfirmButton: false,
+      focusConfirm: false,
+    });
+    Swal.showLoading();
+  }
+}
