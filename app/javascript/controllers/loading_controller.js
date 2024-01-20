b3d929ef22f6a6ad7af29f133f5df225fde05e84
EnnisHong <kugi7821@gmail.com>
2024-01-20 14:38:57 +0800

    fix: payment button link

--- app/javascript/controllers/loading_controller.js
+++ app/javascript/controllers/loading_controller.js
@@ -1,21 +1,22 @@
-import { Controller } from "@hotwired/stimulus";
-import Swal from "sweetalert2";
+import { Controller } from "@hotwired/stimulus"
+import Swal from "sweetalert2"
 
 // Connects to data-controller="loading"
 export default class extends Controller {
   disconnect() {
-    Swal.close();
+    Swal.close()
   }
 
   loading() {
-    const { message } = this.element.dataset;
+    const { message } = this.element.dataset
+
     Swal.fire({
       title: message,
       allowEscapeKey: false,
       allowOutsideClick: false,
       showConfirmButton: false,
       focusConfirm: false,
-    });
-    Swal.showLoading();
+    })
+    Swal.showLoading()
   }
 }
