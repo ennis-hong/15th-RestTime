e1f0212b8213700d61fcc085e8b657a6241ec6bb
艸先生 <kauia96@gmail.com>
2024-01-24 00:49:33 +0800

    fix:rubocop

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -15,8 +15,8 @@ class ShopsController < ApplicationController
 
     puts "Latitude: #{params[:latitude]}, Longitude: #{params[:longitude]}"
 
-    @nearby_shops = (@latitude.present? && @longitude.present?) && Shop.near([@latitude, @longitude], radius) || []
-    
+    @nearby_shops = ((@latitude.present? && @longitude.present?) && Shop.near([@latitude, @longitude], radius)) || []
+
     @q = Shop.ransack(params[:q])
     @shops = @q.result(distinct: true).order(order_by).status.page(params[:page])
   end
