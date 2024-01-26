c082f2bcaa9fc291a2bc464f72fae5468f606574
EnnisHong <kugi7821@gmail.com>
2024-01-26 10:45:37 +0800

    fix:營業時間未填寫訊息

--- app/models/service_time.rb
+++ app/models/service_time.rb
@@ -9,13 +9,14 @@ class ServiceTime < ApplicationRecord
   def validate_open_and_close_times
     return if off_day
 
+    return errors.add(:base, :time_blank) if  open_time.blank? && close_time.blank?
+
     if open_time.present? && close_time.blank?
       errors.add(:base, :close_time_blank)
       return
     end
 
     return unless close_time.present? && open_time.blank?
-
     errors.add(:base, :open_time_blank)
   end
 end
