91aa267d74b905448e3443f467be3a30c951534f
EnnisHong <kugi7821@gmail.com>
2024-01-26 10:47:19 +0800

    chore: scan with rubocop

--- app/models/service_time.rb
+++ app/models/service_time.rb
@@ -9,7 +9,7 @@ class ServiceTime < ApplicationRecord
   def validate_open_and_close_times
     return if off_day
 
-    return errors.add(:base, :time_blank) if  open_time.blank? && close_time.blank?
+    return errors.add(:base, :time_blank) if open_time.blank? && close_time.blank?
 
     if open_time.present? && close_time.blank?
       errors.add(:base, :close_time_blank)
@@ -17,6 +17,7 @@ class ServiceTime < ApplicationRecord
     end
 
     return unless close_time.present? && open_time.blank?
+
     errors.add(:base, :open_time_blank)
   end
 end
